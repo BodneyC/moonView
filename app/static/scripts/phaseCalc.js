dc4d59e5a91b5bc4d18b5c40298854974fdef999
Benjamin Carrington <benjcarrington@gmail.com>
2018-12-28 10:32:09 +0000

    Improved phaseCalc forumla

--- app/static/scripts/phaseCalc.js
+++ app/static/scripts/phaseCalc.js
@@ -1,54 +1,56 @@
-function getJulian(date) {
-    const daysInYear = 362.25
-    
-    var a = Math.floor(date.getFullYear() / 100)
-    var b = 2 - a + Math.floor(a / 4)
-
-    var e = daysInYear * (date.getFullYear() + 4716)
-    var f = 30.6001 * (date.getMonth() + 1)
-    return b + date.getDay() + e + f - 1524.5
-}
-
 function retMoonName(phaseNum) {
     switch(phaseNum) {
+        case(0):
+            return 'New Moon'
         case(1):
-            return 'newMoon.png'
+            return 'Waxing Crescent'
         case(2):
-            return 'waxingCrescent.png'
+            return 'Quarter'
         case(3):
-            return 'quarters.png'
+            return 'Waxing Gibbous'
         case(4):
-            return 'waxingGibbous.png'
+            return 'Full Moon'
         case(5):
-            return 'fullMoon.png'
+            return 'Waning Gibbous'
         case(6):
-            return 'waxingGibbous.png'
+            return 'Last Quarter'
         case(7):
-            return 'quarters.png'
-        case(8):
-            return 'waxingCrescent.png'
+            return 'Waning Crescent'
     }
 }
 
-(function() {
-    // Calcaulate moon phase information
-    const jdToNewMoon = 2451549.5
-    const daysOfCycle = 29.53
-    const today = new Date()
-    const julian = getJulian(today)
-    const sinceNew = julian - jdToNewMoon
-    const daysIntoCycle = ((sinceNew / daysOfCycle) - Math.floor(sinceNew / daysOfCycle)) * daysOfCycle
-    const percent = (daysIntoCycle / daysOfCycle) * 100
+// Based on: https://gist.github.com/endel/dfe6bb2fbe679781948c
+function getPercent(today) {
+    var day = today.getDay();
+    var month = today.getMonth();
+    var year = today.getFullYear();
 
+    if (month < 3) {
+      year--;
+      month += 12;
+    }
+
+    ++month;
+    var c = 365.25 * year;
+    var e = 30.6 * month;
+    var jd = c + e + day - 694039.09; // jd is total days elapsed
+    jd /= 29.5305882; // divide by the moon cycle
+    var b = parseInt(jd); // int(jd) -> b, take integer part of jd
+    jd -= b; // subtract integer part to leave fractional part of original jd
+
+    return jd;
+}
+
+(function() {
     // Bring in configuration
     const config = JSON.parse(require('fs').readFileSync(__dirname + '/config/config.json', 'utf8'))
-    const dateDDMMYYYY = today.toLocaleDateString()
 
-    // Set background image
-    var i = 1
-    for(; i <= 8; i++)
-        if(percent <= 12.5 * i)
-            break;
-    const phaseNum = i
-    const moonImgName = retMoonName(phaseNum)
+    // Calcaulate moon phase information
+    const percent = getPercent(new Date());
+
+    phase = Math.round(percent * 8); // scale fraction from 0-8 and round
+    if(phase >= 8)
+        phase = 0
+    
+    const moonName = retMoonName(phase);
 })()
