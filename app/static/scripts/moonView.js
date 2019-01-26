c7ba9dbc82a1165dd417491feaf6e2a810887ca6
Benjamin Carrington <benjcarrington@gmail.com>
2019-01-26 15:44:25 +0000

    Added stars

--- app/static/scripts/moonView.js
+++ app/static/scripts/moonView.js
@@ -1,30 +1,8 @@
 var lune = require('lune')
 var currentPhase = lune.phase()
 
-function retMoonName(phaseNum) {
-    switch(phaseNum) {
-        case(0):
-            return 'New Moon'
-        case(1):
-            return 'Waxing Crescent'
-        case(2):
-            return 'Quarter'
-        case(3):
-            return 'Waxing Gibbous'
-        case(4):
-            return 'Full Moon'
-        case(5):
-            return 'Waning Gibbous'
-        case(6):
-            return 'Last Quarter'
-        case(7):
-            return 'Waning Crescent'
-    }
-}
-
-const moonName = retMoonName(parseInt(currentPhase.phase * 8))
+document.getElementById("moontitle").innerHTML = "HELLO"
 
 module.exports = {
-    currentPhase,
-    moonName
+    currentPhase
 }
--- app/static/scripts/paperMoon.js
+++ app/static/scripts/paperMoon.js
@@ -4,10 +4,10 @@ var moonInfo = require(__dirname + '/static/scripts/moonView')
 var moonCol = '#f7f7a3'
 var crescentCol = '#c4c497'
 
-var handleScale = 0.52
+var handleScale = 0.55
 var widthMid = view.size.width / 2
 var heightMid = view.size.height / 2
-var cirLen = widthMid / 2
+var cirLen = widthMid / 4
 var moon = new Path.Circle({
 	center: view.center,
 	radius: cirLen
@@ -27,11 +27,12 @@ function placeCircles() {
 	heightMid = view.size.height / 2
 
 	var oldCirLen = cirLen
-	cirLen = widthMid / 2
+	cirLen = widthMid / 4
 
 	var ratio = cirLen / oldCirLen
 
 	moon.scale(ratio)
+	// moon.fullySelected = true
 
 	handleLen = cirLen * handleScale
 	vector.length = handleLen
@@ -46,14 +47,12 @@ function placeCircles() {
 
 	// Probably un-needed when the onMouseMove() goes
 	var curve = crescent.curves
-	curve[1].handle1.x = curve[2].handle2.x = (view.center.x - cirLen) * handleScale
+	curve[1].handle1.x = curve[2].handle2.x = handleLen
 
-	var delta = cirLen * handleScale
-	var tmpCirLen = view.center.x - cirLen
-	curve[2].handle1.y = delta + ((tmpCirLen - delta) * 0.12)
+	curve[2].handle1.y = handleLen
 	curve[1].handle2.y = -curve[2].handle1.y
 
-	crescent.fullySelected = false
+	// crescent.fullySelected = true
 }
 
 function setCrescentCurve(xVal) {
@@ -78,6 +77,8 @@ function setCrescentCurve(xVal) {
 }
 
 function setCrescentLocation() {
+	moon.position = view.center
+
 	for(var i = 0; i < 4; i++) {
 		crescent.segments[i].point.x = moon.segments[i].point.x
 		crescent.segments[i].point.y = moon.segments[i].point.y
@@ -102,27 +103,54 @@ function getPercentAndQuarter(percent) {
 
 function setCrescentRotateAndColors(info) {
 	if(info.quarter == 0 || info.quarter == 2)
-		crescent.rotate(180, view.center)
+		crescent.rotate(180 + 23.5, view.center)
 	if(info.quarter == 1 || info.quarter == 2) {
 		moon.fillColor = crescentCol
 		crescent.fillColor = moonCol
 	}
-	
+}
+
+var n_stars = view.size.width / 10
+var max_radius = 3
+var stars = []
+
+for(var i = 0; i < n_stars; i++)
+	stars.push(return_star())
+
+function return_star() {
+	return new Path.Circle({
+			center: new Point(
+				Math.floor((Math.random() * view.size.width) + 1),
+				Math.floor((Math.random() * view.size.height) + 1)
+			),
+			radius: Math.floor((Math.random() * max_radius) + 1),
+			fillColor: '#fffcdd'
+		})
 }
 
 function draw() {
 	placeCircles()
-	moon.position = view.center
 	setCrescentLocation()
-	var quarterPercent = getPercentAndQuarter(moonInfo.currentPhase.phase)
+	var quarterPercent = getPercentAndQuarter(moonInfo.currentPhase.illuminated)
 
-	// NOTE: Set curve before rotating (and rotate around view.center)
 	setCrescentCurve(quarterPercent.percent * cirLen)
 	setCrescentRotateAndColors(quarterPercent)
+
+	moon.bringToFront()
+	crescent.bringToFront()
+}
+function redraw_stars() {
+	for(var i = 0; i < n_stars; i++)
+		stars[i].remove()
+	stars = []
+	n_stars = view.size.width / 10
+	for(i = 0; i < n_stars; i++)
+		stars.push(return_star())
 }
 
 function onResize(event) {
-	draw();
+	redraw_stars()
+	draw()
 }
 
 draw()
